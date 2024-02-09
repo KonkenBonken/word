fd90ba4202f24a2a493561e5bb13c1c0147342a0
KonkenBonken <konrad05pettersson@gmail.com>
2024-02-09 20:48:29 +0100

    store relative frequencies

--- graph.py
+++ graph.py
@@ -13,5 +13,10 @@ with open('ord.txt', 'r', encoding='utf8') as f:
         for i in range(len(word)-1):
             chars[word[i]][word[i+1]] += 1
 
+for a in chars.keys():
+    tot = sum(chars[a].values())
+    for b in chars[a].keys():
+        chars[a][b] /= tot
+
 with open('graph.pickle', 'wb') as f:
     pickle.dump(chars, f)
