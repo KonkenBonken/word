1976c9985b50434f69c4f7efe0066f9c70d4d79c
KonkenBonken <konrad05pettersson@gmail.com>
2024-02-09 20:43:53 +0100

    replace graph with frequency dict

--- graph.py
+++ graph.py
@@ -1,6 +1,9 @@
 import pickle
 
-chars = dict([c, set()] for c in 'abcdefghijklmnopqrstuvwxyzåäö ')
+chars = dict(
+    [a, dict([b, 0] for b in 'abcdefghijklmnopqrstuvwxyzåäö ')]
+    for a in 'abcdefghijklmnopqrstuvwxyzåäö ')
+
 
 with open('ord.txt', 'r', encoding='utf8') as f:
     while True:
@@ -8,7 +11,7 @@ with open('ord.txt', 'r', encoding='utf8') as f:
         if not word:
             break
         for i in range(len(word)-1):
-            chars[word[i]].add(word[i+1])
+            chars[word[i]][word[i+1]] += 1
 
 with open('graph.pickle', 'wb') as f:
     pickle.dump(chars, f)
